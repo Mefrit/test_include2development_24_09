d38c77871037254ab3149be8f4f277d5a12c1b6b
Mihail <mihail_fokin_1999@mail.ru>
2020-09-24 08:02:28 +0300

    change greeting mesasge

--- HelloCourse.java
+++ HelloCourse.java
@@ -1,7 +1,7 @@
 class HelloCourse {
 	public static void main(String[] args) {
 		System.out.println("A");
-		System.out.println("Hello industrial development course!");
+		System.out.println("Hello   2");
 		System.out.println("B");
 	}
 }
